d7166c4aa984193f97d04dcdcc8ecdf0770e4210
unknown <manthan@sarvadhi.com>
2021-09-30 16:36:18 +0530

    change in MessageConsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -99,13 +99,15 @@ class MessageConsumer(AsyncJsonWebsocketConsumer):
             self.room_name = "room" + str(self.room_id)
             message_data = await self.send_message_data(self.room_id, self.user_id, message)
 
-            if message_data['result'] == 'false':
-                await self.send_json(message_data)
+            # if message_data['result'] == 'false':
+                # await self.send_json(message_data)
             # elif message_data['result'] == 'room_false':
             #     await self.send_json(message_data)
-            else:
-                message_data = await self.get_message_data(self.room_id, self.user_id)
-                await self.send_json(message_data)
+            # else:
+                # message_data = await self.get_message_data(self.room_id, self.user_id)
+                # await self.send_json(message_data)
+                
+            await self.send_json(message_data)
 
             await self.channel_layer.group_send(
                 self.room_name,
@@ -133,8 +135,18 @@ class MessageConsumer(AsyncJsonWebsocketConsumer):
         Participants_obj = Participants.objects.get(room=Room_obj)
         users = Participants_obj.users.all()
         if User_obj in users:
-            Messages.objects.create(room=Room_obj,user=User_obj,message=message)
-            result = {'result': 'true', 'Message': 'message data enter in database', 'internalCode': '003'}
+            Messages_obj = Messages.objects.create(room=Room_obj,user=User_obj,message=message)
+            Messages_obj_dic = {}
+            Messages_obj_dic['message_id'] = Messages_obj.id
+            Messages_obj_dic['room_name'] = Messages_obj.room.room_name
+            Messages_obj_dic['username'] = Messages_obj.user.username
+            Messages_obj_dic['message'] = ''
+            Messages_obj_dic['file'] = ''
+            if Messages_obj.file:
+                Messages_obj_dic['file'] = Messages_obj.file.url
+            if Messages_obj.message:
+                Messages_obj_dic['message'] = Messages_obj.message
+            result = {'result': 'true', 'Message': Messages_obj_dic, 'internalCode': '003'}
         else:
             result = {'result': 'false', 'Message': 'user id does not in group', 'internalCode': '005'}
         return result
